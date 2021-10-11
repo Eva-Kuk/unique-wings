2065f7197f17cdbcc30ab9f8476f7643a51afe9d
Evka K <kuklaewa@gmail.com>
2021-10-11 15:24:15 +0000

    feat:add profile form, views and updated template

--- profiles/models.py
+++ profiles/models.py
@@ -16,16 +16,6 @@ class UserProfile(models.Model):
         max_length=20,
         null=True,
         blank=True)
-    default_country = CountryField(
-        blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(
-        max_length=20,
-        null=True,
-        blank=True)
-    default_town_or_city = models.CharField(
-        max_length=40,
-        null=True,
-        blank=True)
     default_street_address1 = models.CharField(
         max_length=80,
         null=True,
@@ -34,8 +24,20 @@ class UserProfile(models.Model):
         max_length=80,
         null=True,
         blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40,
+        null=True,
+        blank=True)
     default_county = models.CharField(
         max_length=80,
+        null=True,
+        blank=True)
+    default_postcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True)
+    default_country = CountryField(
+        blank_label='Country',
         null=True,
         blank=True)
 
