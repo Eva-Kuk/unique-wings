7317dd2c010c4f1b52831cd1a4ba1d573a71a9c4
Evka K <kuklaewa@gmail.com>
2021-10-18 16:47:15 +0000

    feat:add image field to edit_blog for superuser

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import BlogPost, BlogComment
 
 
@@ -13,6 +14,11 @@ class BlogForm(forms.ModelForm):
             'content',
             'image',
             )
+
+    image = forms.ImageField(
+        label='Image',
+        required=False,
+        widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
