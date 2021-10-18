604cb3cc5d1ad1a2089be762349ad73bf6ac450a
Evka K <kuklaewa@gmail.com>
2021-10-18 20:33:19 +0000

    feat:add comment form to blog

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import BlogPost, BlogComment
 
 
 class BlogForm(forms.ModelForm):
-    """ Create form for blog posts """
+    """ Create BlogForm for blog posts """
 
     class Meta:
         model = BlogPost
@@ -24,3 +24,13 @@ class BlogForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class CommentForm(forms.ModelForm):
+    """ Create CommentForm for blog posts """
+
+    class Meta:
+        model = BlogComment
+        fields = (
+            'comment',
+            )
