da2a380eaabc95de2c5d3ffd8d47ecf541f85a97
Evka K <kuklaewa@gmail.com>
2021-10-27 17:31:03 +0000

    add deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn unique-wings.wsgi:application
--- unique_wings/settings.py
+++ unique_wings/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a-s8)y91q1ts+seem3o(-2fzhd9o!ft2hpq-tb6w(da%aihome
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['unique-wings.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,13 +124,17 @@ WSGI_APPLICATION = 'unique_wings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
