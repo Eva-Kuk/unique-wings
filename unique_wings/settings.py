553d292a42ee5e730f48eb4e785f8f08c1b785d7
Evka K <kuklaewa@gmail.com>
2021-10-27 19:48:43 +0000

    add removed secret key, set debug and deleted reqiurements file

--- unique_wings/settings.py
+++ unique_wings/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a-s8)y91q1ts+seem3o(-2fzhd9o!ft2hpq-tb6w(da%aihome'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['unique-wings.herokuapp.com', 'localhost']
 
