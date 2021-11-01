b97b8e065f7c925763e0667bf8b5afc01ef0ffee
Evka K <kuklaewa@gmail.com>
2021-11-01 11:40:11 +0000

    add forms and templates for Review app

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .widgets import CustomClearableFileInput
-from .models import Product, Category
+from .models import Product, Category, Review
 
 
 class ProductForm(forms.ModelForm):
@@ -9,7 +9,10 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(
+        label='Image',
+        required=False,
+        widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -19,3 +22,39 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = (
+            'user',
+            'date_posted',
+            'product',
+        )
+
+        fields = (
+            'comment',
+            'rate'
+        )
+
+        labels = {
+            'rate': 'Rate',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'comment': 'Comment',
+        }
+
+        # Add placeholders and classes to input fields
+        self.fields['comment'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'rate':
+                placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
+
+            self.fields[field].widget.attrs['class'] = (
+                'mb-3 rounded-0')
